ddcc390479f77a47f8884b23f9c6c59480b4a12c
Aryamirsepasi <developer@aryamirsepasi.com>
2025-11-08 22:13:40 +0100

    fixed bug that caused onboarding to always appear

--- WritingToolsKeyboard/WritingToolsKeyboard/Views/OnboardingView.swift
+++ WritingToolsKeyboard/WritingToolsKeyboard/Views/OnboardingView.swift
@@ -4,11 +4,7 @@ import CoreFoundation
 
 struct OnboardingView: View {
   @Environment(\.dismiss) private var dismiss
-  @AppStorage(
-    "hasCompletedOnboarding",
-    store: UserDefaults(suiteName: "group.com.aryamirsepasi.writingtools")
-  ) private var hasCompletedOnboarding: Bool = false
-    
+  @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding: Bool = false
   @State private var currentPage: Int = 0
 
   // Setup status
